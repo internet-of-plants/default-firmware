58b97aefc1ed2d7d5683df8f2ba15a9587123ff3
Paulo Cabral Sanz <paulosanz@poli.ufrj.br>
2023-03-30 04:59:07 -0300

    Cleanup default firmware

--- src/main.cpp
+++ src/main.cpp
@@ -3,8 +3,7 @@
 #include "generated/psk.hpp"
 
 namespace config {
-constexpr static iop::time::milliseconds measurementsInterval = 180 * 1000;
-constexpr static iop::time::milliseconds unauthenticatedActionsInterval = 1000;
+constexpr static iop::time::milliseconds interval = 180 * 1000;
 constexpr static int8_t timezone = -3;
 constexpr static char SSID_RAW[] IOP_ROM = "iop";
 static const iop::StaticString SSID = reinterpret_cast<const __FlashStringHelper*>(SSID_RAW);
@@ -12,33 +11,22 @@ static const iop::StaticString PSK = reinterpret_cast<const __FlashStringHelper*
 }
 
 // Sends empty json object to internet-of-plants server, updating the firmware if possible
-auto prepareJson(iop::EventLoop & loop) noexcept -> std::unique_ptr<iop::Api::Json> {
+auto prepareJson(iop::EventLoop & loop) noexcept -> iop::Api::Json {
   IOP_TRACE();
 
-  loop.logger().infoln(IOP_STR("Handle Measurements"));
-  auto json = loop.api().makeJson(IOP_FUNC, [](JsonDocument &doc) {
-    (void) doc;
-  });
+  auto json = loop.api().makeJson(IOP_FUNC, [](JsonDocument &doc) { (void) doc; });
   iop_assert(json, IOP_STR("Unable to generate request payload, OOM or buffer overflow"));
   return json;
 }
 
-auto reportMeasurements(iop::EventLoop &loop, const iop::AuthToken &token) noexcept -> void {
-  loop.registerEvent(token, *prepareJson(loop));
-}
-
-auto cleanup() {}
-
-auto unauthenticatedAct(iop::EventLoop &loop) noexcept -> void {
-  (void) loop;
+auto contactServer(iop::EventLoop &loop, const iop::AuthToken &token) noexcept -> void {
+  loop.registerEvent(token, prepareJson(loop));
 }
 
 namespace iop {
 auto setup(EventLoop &loop) noexcept -> void {
   loop.setAccessPointCredentials(config::SSID, config::PSK);
   loop.setTimezone(config::timezone);
-  loop.setCleanup(cleanup);
-  loop.setInterval(config::unauthenticatedActionsInterval, unauthenticatedAct);
-  loop.setAuthenticatedInterval(config::measurementsInterval, reportMeasurements);
+  loop.setAuthenticatedInterval(config::interval, contactServer);
 }
 }
